5ccab21a5030b956a65f4b7bd8284688fcbeb636
mash017 <mashrafiabrar052@gmail.com>
2024-08-29 20:35:27 +0600

    Create 1049 - Animal.c

---
+++ 1049 - Animal.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+  char a[10];
+  char b[10];
+  char c[10];
+   scanf("%s", &a);
+   scanf("%s", &b);
+   scanf("%s", &c);
+   if (a[0] == 'v' && b[0] == 'a' && c[0] == 'c')
+   printf("aguia\n");
+    else if (a[0] == 'v' && b[0] == 'a' && c[0] == 'o')
+   printf("pomba\n");
+  else if (a[0]=='v'&&b[0]=='m'&&c[0]=='o')
+   printf("homem\n");
+   else if (a[0]=='v'&&b[0]=='m'&&c[0]=='h')
+   printf("vaca\n");
+   else if(a[0]=='i'&&b[0]=='i'&&c[2]=='m')
+   printf("pulga\n");
+  else if (a[0]=='i'&&b[0]=='i'&&c[2]=='r')
+   printf("lagarta\n");
+   else if (a[0]=='i'&&b[0]=='a'&&c[0]=='h')
+   printf("sanguessuga\n");
+  else if (a[0]=='i'&&b[0]=='a'&&c[0]=='o')
+   printf("minhoca\n");
+return 0;   
+
+}
